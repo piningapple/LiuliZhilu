d861dc211d4a50f5a15e6b902f7a6c908be05f78
Ana <dns80791@gmail.com>
2025-11-03 17:09:16 +0800

    added api translation test

--- tests/db_test.py
+++ tests/db_test.py
@@ -1,5 +1,12 @@
 import pytest
 from database_dict import get_translation_with_examples
+import requests
+from fastapi.testclient import TestClient
+from server import app
+import pytest
+
+client = TestClient(app)
+
 
 
 def test_translate_pinyin():
@@ -24,8 +31,23 @@ def test_translate_examples():
     keys = list(definitions.keys())
     examples = definitions[keys[0]]
     
-    assert len(examples) > 0
-    
+    assert len(examples) > 0    
     assert '中国' in examples[0] 
     
+    
+def test_translate_api():
+    char = '中国'
+    response = client.get("/api/translate/?ch="+char)
+    assert response.status_code == 200
+    
+    data = response.json()
+    
+    assert data != ""
+    assert 'character' in data
+    assert 'pinyin' in data
+    assert 'definitions' in data
+    assert len(data['definitions']) > 0
+    
+    keys = list(data['definitions'].keys())
+    assert len(data['definitions'][keys[0]]) > 0
     
