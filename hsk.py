acd293e559c3457a0830eacd1a05388cfd6c1cef
Ana <dns80791@gmail.com>
2026-04-07 00:45:28 +0800

    new hsk

--- hsk.py
+++ hsk.py
@@ -1,4 +1,5 @@
 import re
+import csv
 import os.path
 import pandas as pd
 from pinyin import get_seg_text
@@ -32,27 +33,74 @@ def read_lines_to_list(filename):
     return lines
 
 def str_split(filename):
-    with open(filename, 'r', encoding='utf-8') as file:
-        word_count, raw_words = file.readlines()      
-        raw_words = raw_words[1:]
-        words = []
 
-        for i in range(2,int(word_count)+1):
-            word, raw_words = raw_words.split(str(i), 1)
-            words.append(word)
-            print(word)
+    hsk = {
+                1: [],
+                2: [],
+                3: [],
+                4: [],
+                5: [],
+                6: []
+            }
+    
+    word_in_count = 0
+    
+    for i in range(1,7):
+        with open(f'{filename}hsk{i}_raw.txt', 'r', encoding='utf-8') as file:
+            word_count, raw_words = file.readlines()  
+            raw_words = f' {raw_words.split(' ',1)[1]}'
+            
 
-        print(raw_words)
+            words = []
 
-        words.append(raw_words)
-        print(len(words))
+            for j in range(word_in_count+2,word_in_count+int(word_count)+1):
+                word, raw_words = raw_words.split(str(j), 1)
+                words.append(word[1:])
+
+            words.append(raw_words[1:])
 
 
-        for w in words:
-            if  "(" in w[0]:
+            print(f"hsk {i}: {len(words)}/{word_count}")
+            word_in_count += len(words)
+        
+
+            for w in words:
+                parts = w.split()
+                parts[1] = re.sub(r'\d+', '', parts[1])
+
+                if '（' in parts[0]:
+                    number = re.findall(r'（([^）]+)）', parts[0])
+                    fu = re.findall(r'（([^）]+)）', parts[3])
+                    hsk[i].append(f'{parts[0][0]} {parts[1]} {parts[2]} {parts[3].split('、')[0]}')
+                    for k in range (0,len(number)):
+                        if number[k]!="7-9": 
+                            hsk[int(number[k])].append(f"{number[k]} {parts[1]} {parts[2]} {fu[k]}")
+                else:
+                    hsk[i].append(f"{parts[0]} {parts[1]} {parts[2]} {parts[3] if len(parts) > 3 else ''}")
+
+    
+    
+    for i in range(1,7):  
+        uncleaned_hsk = hsk[i]
+        hsk[i] = list(filter(None, uncleaned_hsk))
+        for w in hsk[i]:
+            hsk[i] = w.split()                 
+
+        with open(f'./data/hsk/hsk{i}_w.csv', "w", newline="",encoding='utf-8') as file:
+            writer = csv.writer(file)
+            writer.writerow(["№","Иероглиф", "Пиньинь", "Часть речи"])
+            writer.writerows(hsk[i])
+
+
+
+        
+
+
+
+
                 
 
-str_split(f'./data/hsk/hsk1_raw.txt')
+str_split('./data/hsk/')
 
 def text_to_csv():
     for i in range(1,7):
--- tools.md
+++ tools.md
@@ -7,5 +7,10 @@ https://github.com/fxsjy/jieba
     https://www.lancaster.ac.uk/fass/projects/corpus/ZCTC/annotation.html 
 - dragonmapper для добавления пиньиня
 https://github.com/tsroten/dragonmapper?tab=readme-ov-file
-
+- Universal Dependencies (UD): Это проект, где есть размеченные корпуса на многих языках, включая китайский (UD_Chinese-GSD, UD_Chinese-CFL). Данные размечены по единому стандарту (слова, части речи, синтаксические зависимости).
+Что должно быть в данных:
+Предложение, разбитое на слова (сегментация).
+Для каждого слова — его часть речи (POS-тег).
+Для каждого слова — его "родитель" (главное слово, от которого оно зависит) и тип связи (например, nsubj — подлежащее, obj — дополнение, amod — определение и т.д.)
+https://universaldependencies.org/
 
